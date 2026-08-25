fd256a77cbf3cf7755868b397a6bfbcfa5efaba3
unknown <071poudel@gmail.com>
2026-08-25 16:41:31 +0545

    Cardio FastAPI BackEnd

---
+++ app/main.py
@@ -0,0 +1,36 @@
+# API
+'''
+Types of Requests
+------------------
+Get -> Read / Select
+Post -> Create / Insert / Send
+Put -> Update
+Delete -> Remove
+'''
+
+from fastapi import FastAPI
+from app.schema import CardioSchema
+from app.model import load_logistic_model
+import pandas as pd
+
+# Fast API Object
+app = FastAPI()
+model, scaler = load_logistic_model()
+
+# API Endpoints / Requests
+@app.get('/')
+def home():
+    return 'Welcome to cardiovascular disease prediction'
+
+@app.post('/predict-cardio-logistic')
+def predict_cardio(data:CardioSchema):
+    input_data = pd.DataFrame([
+        # Accept data as JSON format
+        data.model_dump()
+    ])
+    input_scaler = scaler.transform(input_data)
+    prediction = model.predict(input_scaler)[0]
+    return {
+        'Prediction Status': int(prediction),
+        'Status': 'Likely to be Healthy' if prediction == 0 else 'Likely to be Unhealthy'
+    }
---
+++ train_model.py
@@ -0,0 +1,6 @@
+from app.model import logistic_cardio_predict
+
+if __name__ == '__main__':
+    logistic_cardio_predict()
+    print('Model trained and saved successfully')
+    
